90720a82d0a6802934f0a1900b5044aebe5ba74d
Fany <fany2607@web.de>
2017-06-06 15:55:28 +0200

    Sortieren der Buttons

--- bildergui.py
+++ bildergui.py
@@ -33,9 +33,6 @@ w = Label(root, text="Bottom", font = "Impact 18").place(x=170, y=280)
 w = Button(root, text="Open", bg="green", width=5, height=1, font="10", fg="white", command=fuck).place(x=370, y=57) #case 7
 w = Button(root, text="Close", bg="red", width=5, height=1, font="10", fg="black", command=fuck).place(x=370,y=98) #case 8
 
-w = Button(root, text="Open", bg="green", width=5, height=1, font="10", fg="white", command=fuck).place(x=370, y=225) #case 7
-w = Button(root, text="Close", bg="red", width=5, height=1, font="10", fg="black", command=fuck).place(x=370,y=265) #case 8
-
 w = Button(root, text="←", bg="white", font="Times 16", width=2, height=1, command=callback).place(x=270,y=10) #case 5
 w = Button(root, text="→", bg="white", font="Times 16", width=2, height=1, command=callback).place(x=320, y=10) #case 6
 
@@ -45,14 +42,20 @@ w = Button(root, text="↓", bg="white", font="Times 16", width=2, height=1, com
 w = Button(root, text="←", bg="white", font="Times 16", width=2, height=1, command=callback).place(x=270, y=135) #case 2
 w = Button(root, text="→", bg="white", font="Times 16", width=2, height=1, command=callback).place(x=320, y=135) #case 1
 
+'''untere Buttons'''
+w = Button(root, text="Open", bg="green", width=5, height=1, font="10", fg="white", command=fuck).place(x=370, y=225) #case 7
+w = Button(root, text="Close", bg="red", width=5, height=1, font="10", fg="black", command=fuck).place(x=370,y=265) #case 8
+
+
 w = Button(root, text="↑", bg="White", font="Times 16", width=2, height=1, command=callback).place(x=270, y=210) #case 9
 w = Button(root, text="↓", bg="white", font="Times 16", width=2, height=1, command=callback).place(x=320, y=210) #case 10
 
 w = Button(root, text="←", bg="white", font="Times 16", width=2, height=1, command=callback).place(x=270, y=280) #case2
 w = Button(root, text="→", bg="white", font="Times 16", width=2, height=1, command=callback).place(x=320, y=280) #case 1
 
-Radiobutton(root, text="Manuell", font="Impact 14", variable=v, value=1).place(x=430,y=83)
-Radiobutton(root, text="Automatisch", font="impact 14", variable=v, value=2).place(x=430, y=245)
+
+Radiobutton(root, text="Manuell", font="Impact 14", variable=v, value=1).place(x=430,y=83)    #wenn diese aktiviert ist, dann sollen die unteren buttons nicht mehr gehen
+Radiobutton(root, text="Automatisch", font="impact 14", variable=v, value=2).place(x=430, y=245) #wenn dieses aktiviert ist sollen oberen buttons nicht mehr gehen
 
 root.mainloop()
 

a32058875c30adb6f856563c4100b7cf05f0da80
Fany <fany2607@web.de>
2017-06-07 10:03:17 +0200

    funktionen funktionieren noch nicht

--- bildergui.py
+++ bildergui.py
@@ -13,7 +13,41 @@ def callback():
 	
 def fuck():
     print("MOTHERFUCKER!")
-
+	
+'''test'''
+def black():
+ a(state='enabled') #funktioniert noch nciht 
+ b(state='enabled')
+ c(state='enabled')
+ d(state='enabled')
+ e(state='enabled')
+ f(state='enabled')
+ 
+ h(state = 'disabled')
+ i(state = 'disabled')
+ j(state = 'disabled')
+ k(state = 'disabled')
+ l(state = 'disabled') 
+ m(state = 'disabled')
+ n(state = 'disabled')
+ o(state = 'disabled')
+def normal():
+ a(state='disabled') #funktioniert noch nciht 
+ b(state='disabled')
+ c(state='disabled')
+ d(state='disabled')
+ e(state='disabled')
+ f(state='disabled')
+ 
+ h(state = 'enabled')
+ i(state = 'enabled')
+ j(state = 'enabled')
+ k(state = 'enabled')
+ l(state = 'enabled') 
+ m(state = 'enabled')
+ n(state = 'enabled')
+ o(state = 'enabled')
+ 
 skizze= PhotoImage(file="C:/Users/Waleriya/Desktop/skizze.gif")
 skizze2 = PhotoImage(file="C:/Users/Waleriya/Desktop/skizze2.gif")
 
@@ -30,32 +64,32 @@ w = Label(root, text="Bottom", font = "Impact 18").place(x=170, y=280)
 
 
 
-w = Button(root, text="Open", bg="green", width=5, height=1, font="10", fg="white", command=fuck).place(x=370, y=57) #case 7
-w = Button(root, text="Close", bg="red", width=5, height=1, font="10", fg="black", command=fuck).place(x=370,y=98) #case 8
+h = Button(root, text="Open", bg="green", width=5, height=1, font="10", fg="white", command=fuck).place(x=370, y=57) #case 7
+i = Button(root, text="Close", bg="red", width=5, height=1, font="10", fg="black", command=fuck).place(x=370,y=98) #case 8
 
-w = Button(root, text="←", bg="white", font="Times 16", width=2, height=1, command=callback).place(x=270,y=10) #case 5
-w = Button(root, text="→", bg="white", font="Times 16", width=2, height=1, command=callback).place(x=320, y=10) #case 6
+j = Button(root, text="←", bg="white", font="Times 16", width=2, height=1, command=callback).place(x=270,y=10) #case 5
+k = Button(root, text="→", bg="white", font="Times 16", width=2, height=1, command=callback).place(x=320, y=10) #case 6
 
-w = Button(root, text="↑", bg="White", font="Times 16", width=2, height=1, command=callback).place(x=270, y=73) #case 3
-w = Button(root, text="↓", bg="white", font="Times 16", width=2, height=1, command=callback).place(x=320, y=73) #case4
+l = Button(root, text="↑", bg="White", font="Times 16", width=2, height=1, command=callback).place(x=270, y=73) #case 3
+m = Button(root, text="↓", bg="white", font="Times 16", width=2, height=1, command=callback).place(x=320, y=73) #case4
 
-w = Button(root, text="←", bg="white", font="Times 16", width=2, height=1, command=callback).place(x=270, y=135) #case 2
-w = Button(root, text="→", bg="white", font="Times 16", width=2, height=1, command=callback).place(x=320, y=135) #case 1
+n = Button(root, text="←", bg="white", font="Times 16", width=2, height=1, command=callback).place(x=270, y=135) #case 2
+o = Button(root, text="→", bg="white", font="Times 16", width=2, height=1, command=callback).place(x=320, y=135) #case 1
 
 '''untere Buttons'''
-w = Button(root, text="Open", bg="green", width=5, height=1, font="10", fg="white", command=fuck).place(x=370, y=225) #case 7
-w = Button(root, text="Close", bg="red", width=5, height=1, font="10", fg="black", command=fuck).place(x=370,y=265) #case 8
+a = Button(root, text="Open", bg="green", width=5, height=1, font="10", fg="white", command=fuck).place(x=370, y=225) #case 7
+b = Button(root, text="Close", bg="red", width=5, height=1, font="10", fg="black", command=fuck).place(x=370,y=265) #case 8
 
 
-w = Button(root, text="↑", bg="White", font="Times 16", width=2, height=1, command=callback).place(x=270, y=210) #case 9
-w = Button(root, text="↓", bg="white", font="Times 16", width=2, height=1, command=callback).place(x=320, y=210) #case 10
+c = Button(root, text="↑", bg="White", font="Times 16", width=2, height=1, command=callback).place(x=270, y=210) #case 9
+d = Button(root, text="↓", bg="white", font="Times 16", width=2, height=1, command=callback).place(x=320, y=210) #case 10
 
-w = Button(root, text="←", bg="white", font="Times 16", width=2, height=1, command=callback).place(x=270, y=280) #case2
-w = Button(root, text="→", bg="white", font="Times 16", width=2, height=1, command=callback).place(x=320, y=280) #case 1
+e = Button(root, text="←", bg="white", font="Times 16", width=2, height=1, command=callback).place(x=270, y=280) #case2
+f = Button(root, text="→", bg="white", font="Times 16", width=2, height=1, command=callback).place(x=320, y=280) #case 1
 
 
-Radiobutton(root, text="Manuell", font="Impact 14", variable=v, value=1).place(x=430,y=83)    #wenn diese aktiviert ist, dann sollen die unteren buttons nicht mehr gehen
-Radiobutton(root, text="Automatisch", font="impact 14", variable=v, value=2).place(x=430, y=245) #wenn dieses aktiviert ist sollen oberen buttons nicht mehr gehen
-
+Radiobutton(root, text="Manuell", font="Impact 14",  width = 15, variable=v, value=1, command=normal).place(x=430,y=83)    #wenn diese aktiviert ist, dann sollen die unteren buttons nicht mehr gehen
+Radiobutton(root, text="Automatisch", font="impact 14",  width = 15, variable=v, value=2, command=black).place(x=430, y=245) #wenn dieses aktiviert ist sollen oberen buttons nicht mehr gehen
+# mit command state=disable
 root.mainloop()
 

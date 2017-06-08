d438854fed615e7d4e8c123283f154817854b036
Fany <fany2607@web.de>
2017-06-08 22:17:06 +0200

    k

--- bildergui.py
+++ bildergui.py
@@ -8,7 +8,7 @@ root.geometry("600x360")
 
 connected = False
 
-serialPort = "COM7"
+serialPort = "COM3"
 baudRate=9600
 ser = Serial(timeout=1, writeTimeout=0)
 ser.port = serialPort
@@ -19,12 +19,16 @@ ser.open()
 
 v=IntVar()
 
+def on_press(event):
+    print("Button is pressed.")
+	
+def on_release(event):
+    print("Button is released.")
+
 
 def case_1():
     ser.write("a".encode("utf-8"))
-  # ser.write(51)
-  # print(ser.write("3".encode("utf-8")))
-##
+	
 def case_2():
     ser.write("b".encode("utf-8"))
 
@@ -104,41 +108,69 @@ w = Label(root, text="Bottom", font = "Impact 18").place(x=170, y=280)
 
 
 h = Button(root, state=DISABLED, text="Open", bg="green", width=5, height=1, font="10", fg="white", command=case_7)
-h.place(x=370, y=57) #case 7
+h.place(x=370, y=57)
+h.bind("<ButtonPress>", on_press)
+h.bind("<ButtonRelease>", on_release) #case 7
 i = Button(root, state=DISABLED, text="Close", bg="red", width=5, height=1, font="10", fg="black", command=case_8)
-i.place(x=370,y=98) #case 8
+i.place(x=370,y=98) 
+i.bind("<ButtonPress>", on_press)
+i.bind("<ButtonRelease>", on_release)#case 8
 
 j = Button(root, state=DISABLED, text="←", bg="white", font="Times 16", width=2, height=1, command=case_5)
-j.place(x=270,y=10) #case 5
+j.place(x=270,y=10) 
+j.bind("<ButtonPress>", on_press)
+j.bind("<ButtonRelease>", on_release)#case 5
 k = Button(root, state=DISABLED, text="→", bg="white", font="Times 16", width=2, height=1, command=case_6)
-k.place(x=320, y=10) #case 6
+k.place(x=320, y=10)
+k.bind("<ButtonPress>", on_press)
+k.bind("<ButtonRelease>", on_release) #case 6
 
 l = Button(root, state=DISABLED, text="↑", bg="White", font="Times 16", width=2, height=1, command=case_3)
-l.place(x=270, y=73) #case 3
+l.place(x=270, y=73)
+l.bind("<ButtonPress>", on_press)
+l.bind("<ButtonRelease>", on_release) #case 3
 m = Button(root, state=DISABLED, text="↓", bg="white", font="Times 16", width=2, height=1, command=case_4)
-m.place(x=320, y=73) #case4
+m.place(x=320, y=73)
+m.bind("<ButtonPress>", on_press)
+m.bind("<ButtonRelease>", on_release) #case4
 
 n = Button(root, state=DISABLED, text="←", bg="white", font="Times 16", width=2, height=1, command=case_2)
-n.place(x=270, y=135) #case 2
+n.place(x=270, y=135)
+n.bind("<ButtonPress>", on_press)
+n.bind("<ButtonRelease>", on_release) #case 2
 o = Button(root, state=DISABLED, text="→", bg="white", font="Times 16", width=2, height=1, command=case_1)
-o.place(x=320, y=135) #case 1
+o.place(x=320, y=135)
+o.bind("<ButtonPress>", on_press)
+o.bind("<ButtonRelease>", on_release) #case 1
 
 '''untere Buttons'''
 a = Button(root, state=DISABLED, text="Open", bg="green", width=5, height=1, font="10", fg="white", command=case_7)
-a.place(x=370, y=225) #case 7
+a.place(x=370, y=225)
+a.bind("<ButtonPress>", on_press)
+a.bind("<ButtonRelease>", on_release) #case 7
 b = Button(root, state=DISABLED, text="Close", bg="red", width=5, height=1, font="10", fg="black", command=case_8)
-b.place(x=370,y=265) #case 8
+b.place(x=370,y=265)
+b.bind("<ButtonPress>", on_press)
+b.bind("<ButtonRelease>", on_release) #case 8
 
 
 c = Button(root, state=DISABLED, text="↑", bg="White", font="Times 16", width=2, height=1, command=case_9)
-c.place(x=270, y=210) #case 9
+c.place(x=270, y=210)
+c.bind("<ButtonPress>", on_press)
+c.bind("<ButtonRelease>", on_release) #case 9
 d = Button(root, state=DISABLED, text="↓", bg="white", font="Times 16", width=2, height=1, command=case_10)
-d.place(x=320, y=210) #case 10
+d.place(x=320, y=210)
+d.bind("<ButtonPress>", on_press)
+d.bind("<ButtonRelease>", on_release) #case 10
 
 e = Button(root, state=DISABLED, text="←", bg="white", font="Times 16", width=2, height=1, command=case_2)
-e.place(x=270, y=280) #case2
+e.place(x=270, y=280) 
+e.bind("<ButtonPress>", on_press)
+e.bind("<ButtonRelease>", on_release)#case2
 f = Button(root, state=DISABLED, text="→", bg="white", font="Times 16", width=2, height=1, command=case_1)
-f.place(x=320, y=280) #case 1
+f.place(x=320, y=280)
+f.bind("<ButtonPress>", on_press)
+f.bind("<ButtonRelease>", on_release) #case 1
 
 
 

693b338282babab4a6c2d5d78a12bf795fe4beb4
sk0ty <mikevogt1@hotmail.de>
2017-06-13 15:54:07 +0200

    Ohne Wlan, nur aufgeräumt

--- OHNELAN/OHNELAN.ino
+++ OHNELAN/OHNELAN.ino
@@ -1,14 +1,4 @@
 #include <Servo.h>
-//#include <SoftwareSerial.h>
-//include "DumbServer.h"
-
-/* The WiFi shield is connected to
-   the Arduino pins 3 and 2, as the
-   Arduino has only one hardware serial
-   port (pins 0 and 1) we are using a
-   serial port emulated in software. */
-//SoftwareSerial esp_serial(3, 2);
-//EspServer esp_server;
 
 Servo servo1;
 Servo servo2;
@@ -47,26 +37,7 @@ void setup() {
   pinMode(button1, INPUT_PULLUP);
   pinMode(button2, INPUT_PULLUP);
 
-  /* Connect to the wireless network with the name "GDI"
-    and password "password", change these to match
-    your wifi settings.
-    If anything fails begin() will not return.
-    To debug possible problems you can flash a second
-    Arduino with the "Bare Minimum" example,
-    connect the GNDs of the two Arduinos,
-    connect pin 3 or 2 of the Arduino with the Wifi-shield
-    to pin 1(TX) of the other Arduino and use the Serial monitor
-    to see the Wifi commands and error-messages. */
-  /*
-    Serial.println("Starting server...");
-    esp_server.begin(&esp_serial, "GDI", "password", 30303);
-    Serial.println("...server is running");
-    /* Get and print the IP-Address the python program
-       should connect to */
-  /*char ip[16];
-    esp_server.my_ip(ip, 16);
-    Serial.print("My ip: ");
-    Serial.println(ip);*/
+ 
 }
 
 
@@ -155,81 +126,6 @@ void spezial_funktion_runter (int minimalwinkel, Servo *servoname, Servo *servon
 
 
 void loop()
-{ /*
-     // Check if the python program sent commands
-     if(esp_server.available()) {
-     // Read one line of commands
-     String command= esp_server.readStringUntil('\n');
-     // Echo back the command as-is
-     esp_server.print(">");
-     esp_server.println(command);
-  */
-
-  /*
-    while (digitalRead(button1) == 0) {
-      int var = servo1.read();
-      int var2 =servo2.read();
-      if (var < 180) {
-        servo1.write(var + 1);
-        //var2--;
-       //Alternativ: var2=180-var1; 180 kann auch eine Variable sein die man festlegen kann, das ist dann ja
-       //Der "Gesamtwinkel" des Systems.
-        delay(15);
-        servo2.write(var2 +1);
-        Serial.print(var2 +1);
-        Serial.print(" ");
-        Serial.print(var + 1);
-        Serial.print(" ");
-        Serial.println(var+var2); //Die Summe beider sollte stets umgefähr konstant sein bei 180°.
-      }
-      else {
-        //var == 180;
-      }
-    }
-    while (digitalRead(button2) == 0) {
-      int var = servo1.read();
-      int var2 = servo2.read();
-      if (var > 11) {
-        servo1.write(var - 1);
-        //var2++;
-        delay(15);
-        servo2.write(var2 - 1);
-        Serial.print(var2 - 1);
-        Serial.print(" ");
-        Serial.print(var - 1);
-        Serial.print(" ");
-        Serial.println(var+var2);
-      } else {
-        //var = 11;
-      }
-    }
-  */
-/*
-  if (Serial.available() > 0 ) {
-    speed_var_check = Serial.read();
-    Serial.println(speed_var_check);
-  }
-  switch (speed_var_check) {
-    case 49: speed_var = 5;
-    case 50: speed_var = 6;
-    case 51: speed_var = 7;
-    case 52: speed_var = 8;
-    case 53: speed_var = 9;
-    case 54: speed_var = 10;
-    case 55: speed_var = 11;
-    case 56: speed_var = 12;
-    case 57: speed_var = 13;
-    case 58: speed_var = 14;
-    case 59: speed_var = 15;
-    case 60: speed_var = 16;
-    case 61: speed_var = 17;
-    case 62: speed_var = 18;
-    case 63: speed_var = 19;
-    case 64: speed_var = 20;
-  }
-  */
-
-
   if (Serial.available() > 0) {
     python_button_var = Serial.read();
 //Serial.println(python_button_var);

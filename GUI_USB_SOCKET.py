7c62d4918bcfa829ff6eab3478e98374021a7127
jchoi170303 <jchoi@uni-bremen.de>
2017-06-14 10:43:48 +0200

    WLAN Verbindung und Socket

---
+++ GUI_USB_SOCKET.py
@@ -0,0 +1,205 @@
+from tkinter import *
+from serial import *
+import socket
+
+root=Tk()
+root.title("Roboterarm")
+root.geometry("680x360")
+
+
+connected = False
+
+serialPort = "COM3"
+baudRate=9600
+ser = Serial(timeout=1., writeTimeout=0)
+ser.port = serialPort
+ser.baudrate = baudRate
+ser.open()
+
+s= socket.socket()
+s.connect(('134.102.28.112', 30303))
+s.setblocking(False)
+
+s.send(b'Hello World\n')
+
+s.recv(1024)
+
+
+p=IntVar()
+
+v=IntVar()
+
+def on_press(event):
+    print("Button is pressed.")
+	
+def on_release(event):
+    print("Button is released.")
+
+'''Buttons'''
+	
+def case_1(event):
+    ser.write("a".encode("utf-8"))
+	
+def case_2(event):
+    ser.write("b".encode("utf-8"))
+
+def case_3(event):
+    ser.write("c".encode("utf-8"))
+	
+def case_4(event):
+    ser.write("d".encode("utf-8"))
+
+def case_5(event):
+    ser.write("e".encode("utf-8"))
+
+def case_6(event):
+    ser.write("f".encode("utf-8"))
+
+def case_7(event):
+    ser.write("g".encode("utf-8"))
+
+def case_8(event):
+    ser.write("h".encode("utf-8"))
+
+def case_9(event):
+    ser.write("i".encode("utf-8"))
+	
+def case_10(event):
+    ser.write("j".encode("utf-8"))
+	
+def case_standard(event):
+    ser.write("k".encode("utf-8"))
+	
+	
+'''Geschwindigkeit'''
+	
+def case_schneller(event):
+    ser.write("1".encode("utf-8"))
+    print("w")
+
+def case_schnell(event):
+    ser.write("2".encode("utf-8"))
+    print("e")
+	
+def case_mittelaf(event):
+    ser.write("3".encode("utf-8"))
+    print("bla")
+	
+def case_mittel(event):
+    ser.write("4".encode("utf-8"))
+ 
+def case_langsamer(event):
+    ser.write("5".encode("utf-8"))
+	
+def case_langsam(event):
+    ser.write("6".encode("utf-8"))
+	
+
+	
+
+
+ 
+skizze= PhotoImage(file="skizze_neu.gif")
+skizze2 = PhotoImage(file="skizze2_neu.gif")
+
+w = Label(root, compound = CENTER, image=skizze).grid(row=0,column=0)
+w = Label(root, compound = CENTER, image=skizze2).grid(row=4,column=0)
+
+w = Label(root, text="Top", font = "Impact 18").place(x=170, y=10)
+w = Label(root, text="Middle", font = "Impact 18").place(x=170, y=73)
+w = Label(root, text="Bottom", font = "Impact 18").place(x=170, y=135)
+
+
+w = Label(root, text="Middle", font = "Impact 18").place(x=170, y=210)
+w = Label(root, text="Bottom", font = "Impact 18").place(x=170, y=280)
+
+tempo = Label(root, text="Speed", font="Impact 15").place(x=520, y= 25) 
+
+'''p = Scale(root, from_=5, to=20, length= 260, bd=0, orient=VERTICAL)#Funktionen
+p.place(x=600, y=50)'''
+
+
+h = Button(root, text="Open", bg="green", width=5, height=1, font="10", fg="white")
+h.place(x=370, y=57)
+h.bind("<ButtonPress>", case_7)
+h.bind("<ButtonRelease>", case_standard) #case 7
+i = Button(root, text="Close", bg="red", width=5, height=1, font="10", fg="black")
+i.place(x=370,y=98) 
+i.bind("<ButtonPress>", case_8)
+i.bind("<ButtonRelease>", case_standard)#case 8
+
+j = Button(root, text="↑", bg="white", font="Times 16", width=2, height=1)
+j.place(x=270,y=10) 
+j.bind("<ButtonPress>", case_6)
+j.bind("<ButtonRelease>", case_standard)#case 5
+k = Button(root, text="↓", bg="white", font="Times 16", width=2, height=1)
+k.place(x=320, y=10)
+k.bind("<ButtonPress>", case_5)
+k.bind("<ButtonRelease>", case_standard) #case 6
+
+l = Button(root, text="↑", bg="White", font="Times 16", width=2, height=1)
+l.place(x=270, y=73)
+l.bind("<ButtonPress>", case_3)
+l.bind("<ButtonRelease>", case_standard) #case 3
+m = Button(root, text="↓", bg="white", font="Times 16", width=2, height=1)
+m.place(x=320, y=73)
+m.bind("<ButtonPress>", case_4)
+m.bind("<ButtonRelease>", case_standard) #case4
+
+n = Button(root, text="←", bg="white", font="Times 16", width=2, height=1)
+n.place(x=270, y=135)
+n.bind("<ButtonPress>", case_2)
+n.bind("<ButtonRelease>", case_standard) #case 2
+o = Button(root, text="→", bg="white", font="Times 16", width=2, height=1)
+o.place(x=320, y=135)
+o.bind("<ButtonPress>", case_1)
+o.bind("<ButtonRelease>", case_standard) #case 1
+
+'''untere Buttons'''
+a = Button(root, text="Open", bg="green", width=5, height=1, font="10", fg="white")
+a.place(x=370, y=225)
+a.bind("<ButtonPress>", case_7)
+a.bind("<ButtonRelease>", case_standard) #case 7
+b = Button(root, text="Close", bg="red", width=5, height=1, font="10", fg="black")
+b.place(x=370,y=265)
+b.bind("<ButtonPress>", case_8)
+b.bind("<ButtonRelease>", case_standard) #case 8
+
+
+c = Button(root, text="↑", bg="White", font="Times 16", width=2, height=1)
+c.place(x=270, y=210)
+c.bind("<ButtonPress>", case_9)
+c.bind("<ButtonRelease>", case_standard) #case 9
+d = Button(root, text="↓", bg="white", font="Times 16", width=2, height=1)
+d.place(x=320, y=210)
+d.bind("<ButtonPress>", case_10)
+d.bind("<ButtonRelease>", case_standard) #case 10
+
+e = Button(root, text="←", bg="white", font="Times 16", width=2, height=1)
+e.place(x=270, y=280) 
+e.bind("<ButtonPress>", case_2)
+e.bind("<ButtonRelease>", case_standard)#case2
+f = Button(root, text="→", bg="white", font="Times 16", width=2, height=1)
+f.place(x=320, y=280)
+f.bind("<ButtonPress>", case_1)
+f.bind("<ButtonRelease>", case_standard) #case 1
+
+
+
+x=Radiobutton(root, text="Snail", font="Fixedsys 12", variable=p, value=1, command= lambda : case_langsam(None))
+x.place(x=520,y=70)
+q=Radiobutton(root, text="SuperSlow", font="Fixedsys 12", variable=p, value=2, command= lambda : case_langsamer(None)) 
+q.place(x=520, y=110)
+r=Radiobutton(root, text="Normal", font="Fixedsys 12", variable=p, value=3, command= lambda : case_mittel(None))  
+r.place(x=520,y=150)
+s=Radiobutton(root, text="SuperNormal", font="Fixedsys 12", variable=p, value=4, command= lambda : case_mittelaf(None))
+s.place(x=520, y=190) 
+t=Radiobutton(root, text="SuperSpeed", font="Fixedsys 12", variable=p, value=5, command= lambda : case_schnell(None))
+t.place(x=520,y=230)
+u=Radiobutton(root, text="Hyperspeed", font="Fixedsys 12", variable=p, value=6, command= lambda : case_schneller(None)) 
+u.place(x=520, y=270)
+
+
+
+root.mainloop()
+
---
+++ WLAN_INO_FINAL/OHNELAN2.0.ino
@@ -0,0 +1,292 @@
+#include <Servo.h>
+#include <SoftwareSerial.h>
+#include "DumbServer.h"
+
+/* The WiFi shield is connected to
+the Arduino pins 3 and 2, as the
+Arduino has only one hardware serial
+port (pins 0 and 1) we are using a
+serial port emulated in software. */
+
+SoftwareSerial esp_serial(3, 2);
+EspServer esp_server;
+
+Servo servo1;
+Servo servo2;
+Servo servo3;
+Servo servo4;
+
+
+int servo_1pin = 8;
+int servo_2pin = 9;
+int servo_3pin = 10;
+int servo_4pin = 11;
+
+int button1 = 3;
+int button2 = 4;
+
+
+int python_button_var;
+int speed_var_check;
+int speed_var = 15;
+
+//int var2 = 90;
+
+void setup() {
+	Serial.begin(9600);
+	esp_serial.begin(9600);
+
+	servo1.attach(servo_1pin);
+	servo2.attach(servo_2pin);
+	servo3.attach(servo_3pin);
+	servo4.attach(servo_4pin);
+
+	int angle = servo1.read();
+	// Serial.println(angle);
+	servo2.write(50);
+
+	pinMode(button1, INPUT_PULLUP);
+	pinMode(button2, INPUT_PULLUP);
+
+	/* Connect to the wireless network with the name "GDI"
+	and password "password", change these to match
+	your wifi settings.
+	If anything fails begin() will not return.
+	To debug possible problems you can flash a second
+	Arduino with the "Bare Minimum" example,
+	connect the GNDs of the two Arduinos,
+	connect pin 3 or 2 of the Arduino with the Wifi-shield
+	to pin 1(TX) of the other Arduino and use the Serial monitor
+	to see the Wifi commands and error-messages. */
+
+	Serial.println("Starting server...");
+	esp_server.begin(&esp_serial, "GDI", "password", 30303);
+	Serial.println("...server is running");
+
+	/* Get and print the IP-Address the python program
+	should connect to */
+
+	char ip[16];
+	esp_server.my_ip(ip, 16);
+
+	Serial.print("My ip: ");
+	Serial.println(ip);
+}
+
+
+
+void arm_hoch(int maximalwinkel, Servo *servoname) {
+	int var = servoname->read();
+	if (var < maximalwinkel) {
+		servoname->write(var + 1);
+		//var2--;
+
+		//Alternativ: var2=180-var1; 180 kann auch eine Variable sein die man festlegen kann, das ist dann ja
+		//Der "Gesamtwinkel" des Systems.
+		delay(speed_var);
+
+
+		// Serial.println(var + 1);
+
+	}
+	else {
+		//var == 180;
+	}
+};
+
+void arm_runter(int minimalwinkel, Servo *servoname) {
+	int var = servoname->read();
+
+	if (var > minimalwinkel) {
+		servoname->write(var - 1);
+		//var2++;
+		delay(speed_var);
+
+
+		// Serial.println(var - 1);
+
+	}
+	else {
+		//var = 11;
+	}
+
+};
+
+void spezial_funktion_hoch(int maximalwinkel, Servo *servoname, Servo *servoname2) {
+	int var = servoname->read();
+	int var2 = servoname2->read();
+	if (var < maximalwinkel) {
+		servoname->write(var + 1);
+		//var2--;
+		//Alternativ: var2=180-var1; 180 kann auch eine Variable sein die man festlegen kann, das ist dann ja
+		//Der "Gesamtwinkel" des Systems.
+		delay(speed_var);
+
+		servoname2->write(var2 + 1);
+
+		// Serial.print(var2 + 1);
+		// Serial.print(" ");
+		// Serial.print(var + 1);
+		//Serial.print(" ");
+		// Serial.println(var + var2); //Die Summe beider sollte stets umgefähr konstant sein bei 180°.
+	}
+	else {
+		//var == 180;
+	}
+}
+
+void spezial_funktion_runter(int minimalwinkel, Servo *servoname, Servo *servoname2) {
+	int var = servoname->read();
+	int var2 = servoname2->read();
+
+	if (var > minimalwinkel) {
+		servoname->write(var - 1);
+		//var2++;
+		delay(speed_var);
+
+		servoname2->write(var2 - 1);
+
+		// Serial.print(var2 - 1);
+		// Serial.print(" ");
+		// Serial.print(var - 1);
+		// Serial.print(" ");
+		// Serial.println(var + var2);
+	}
+	else {
+		//var = 11;
+	}
+
+}
+
+
+
+void loop()
+{
+	// Check if the python program sent commands
+	if (esp_server.available()) {
+		// Read one line of commands
+		String command = esp_server.readStringUntil('\n');
+		// Echo back the command as-is
+		esp_server.print(">");
+		esp_server.println(command)
+
+			/*
+			while (digitalRead(button1) == 0) {
+			int var = servo1.read();
+			int var2 =servo2.read();
+			if (var < 180) {
+			servo1.write(var + 1);
+			//var2--;
+			//Alternativ: var2=180-var1; 180 kann auch eine Variable sein die man festlegen kann, das ist dann ja
+			//Der "Gesamtwinkel" des Systems.
+			delay(15);
+			servo2.write(var2 +1);
+			Serial.print(var2 +1);
+			Serial.print(" ");
+			Serial.print(var + 1);
+			Serial.print(" ");
+			Serial.println(var+var2); //Die Summe beider sollte stets umgefähr konstant sein bei 180°.
+			}
+			else {
+			//var == 180;
+			}
+			}
+			while (digitalRead(button2) == 0) {
+			int var = servo1.read();
+			int var2 = servo2.read();
+			if (var > 11) {
+			servo1.write(var - 1);
+			//var2++;
+			delay(15);
+			servo2.write(var2 - 1);
+			Serial.print(var2 - 1);
+			Serial.print(" ");
+			Serial.print(var - 1);
+			Serial.print(" ");
+			Serial.println(var+var2);
+			} else {
+			//var = 11;
+			}
+			}
+			*/
+			/*
+			if (Serial.available() > 0 ) {
+			speed_var_check = Serial.read();
+			Serial.println(speed_var_check);
+			}
+			switch (speed_var_check) {
+			case 49: speed_var = 5;
+			case 50: speed_var = 6;
+			case 51: speed_var = 7;
+			case 52: speed_var = 8;
+			case 53: speed_var = 9;
+			case 54: speed_var = 10;
+			case 55: speed_var = 11;
+			case 56: speed_var = 12;
+			case 57: speed_var = 13;
+			case 58: speed_var = 14;
+			case 59: speed_var = 15;
+			case 60: speed_var = 16;
+			case 61: speed_var = 17;
+			case 62: speed_var = 18;
+			case 63: speed_var = 19;
+			case 64: speed_var = 20;
+			}
+			*/
+
+
+			if (Serial.available() > 0) {
+				python_button_var = Serial.read();
+				//Serial.println(python_button_var);
+			}
+
+		switch (python_button_var) {
+
+		case 97: arm_hoch(180, &servo1);   break; //a
+		case 98: arm_runter(11, &servo1);  break; //b
+
+		case 99: arm_hoch(180, &servo2);   break; //c
+		case 100: arm_runter(11, &servo2);  break; //d
+
+		case 101: arm_hoch(180, &servo3);   break; //e
+		case 102: arm_runter(11, &servo3);  break; //f
+
+		case 103: arm_hoch(180, &servo4);   break; //g
+		case 104: arm_runter(11, &servo4);  break; //h
+
+		case 105: spezial_funktion_hoch(180, &servo2, &servo3); break; //i
+		case 106: spezial_funktion_runter(11, &servo2, &servo3); break; //j
+
+		case 107: break; //default case //k
+
+
+
+
+
+		case 49: speed_var = 5; break;
+		case 50: speed_var = 8;  break;
+		case 51: speed_var = 11; break;
+		case 52: speed_var = 14; break;
+		case 53: speed_var = 17; break;
+		case 54: speed_var = 20; break;
+		}
+
+
+
+
+
+
+		/*
+		// scan from 0 to 180 degrees
+		for(angle = 10; angle < 180; angle++)
+		{
+		servo.write(angle);
+		delay(15);
+		}
+		// now scan back from 180 to 0 degrees
+		for(angle = 180; angle > 10; angle--)
+		{
+		servo.write(angle);
+		delay(15);
+		} */
+	}

fbac7822c3172550b0b90a85fe6c51d55e09e760
sk0ty <mikevogt1@hotmail.de>
2017-06-14 19:57:20 +0200

    OHNELAN2.0 = WLAN

--- OHNELAN2.0/OHNELAN2.0.ino
+++ OHNELAN2.0/OHNELAN2.0.ino
@@ -1,21 +1,21 @@
 
 #include <Servo.h>
-//#include <SoftwareSerial.h>
-//include "DumbServer.h"
+#include <SoftwareSerial.h>
+#include "DumbServer.h"
 
 /* The WiFi shield is connected to
    the Arduino pins 3 and 2, as the
    Arduino has only one hardware serial
    port (pins 0 and 1) we are using a
    serial port emulated in software. */
-//SoftwareSerial esp_serial(3, 2);
-//EspServer esp_server;
+SoftwareSerial esp_serial(3, 2);
+EspServer esp_server;
 
+int led = 12;
 Servo servo1;
 Servo servo2;
 Servo servo3;
 Servo servo4;
-
 
 int servo_1pin = 8;
 int servo_2pin = 9;
@@ -34,19 +34,33 @@ int speed_var = 15;
 
 void setup() {
   Serial.begin(9600);
-  //esp_serial.begin(9600);
+  esp_serial.begin(9600);
 
   servo1.attach(servo_1pin);
   servo2.attach(servo_2pin);
   servo3.attach(servo_3pin);
   servo4.attach(servo_4pin);
 
-  int angle = servo1.read();
- // Serial.println(angle);
-  servo2.write(50);
+ // int angle = servo1.read();
+  // Serial.println(angle);
+  //servo2.write(50);
 
   pinMode(button1, INPUT_PULLUP);
   pinMode(button2, INPUT_PULLUP);
+pinMode(led, OUTPUT);
+  Serial.println("Starting server...");
+  esp_server.begin(&esp_serial, "Fany", "123hohoho", 30304);
+  Serial.println("...server is running");
+
+
+  /* Get and print the IP-Address the python program
+     should connect to */
+  char ip[16];
+  esp_server.my_ip(ip, 16);
+
+  Serial.print("My ip: ");
+  Serial.println(ip);
+
 
   /* Connect to the wireless network with the name "GDI"
     and password "password", change these to match
@@ -83,7 +97,7 @@ void arm_hoch(int maximalwinkel, Servo *servoname ) {
     delay(speed_var);
 
 
-   // Serial.println(var + 1);
+    // Serial.println(var + 1);
 
   }
   else {
@@ -100,7 +114,7 @@ void arm_runter(int minimalwinkel, Servo *servoname ) {
     delay(speed_var);
 
 
-   // Serial.println(var - 1);
+    // Serial.println(var - 1);
 
   } else {
     //var = 11;
@@ -120,11 +134,11 @@ void spezial_funktion_hoch (int maximalwinkel, Servo *servoname, Servo *servonam
 
     servoname2->write(var2 + 1);
 
-   // Serial.print(var2 + 1);
-   // Serial.print(" ");
-   // Serial.print(var + 1);
+    // Serial.print(var2 + 1);
+    // Serial.print(" ");
+    // Serial.print(var + 1);
     //Serial.print(" ");
-   // Serial.println(var + var2); //Die Summe beider sollte stets umgefähr konstant sein bei 180°.
+    // Serial.println(var + var2); //Die Summe beider sollte stets umgefähr konstant sein bei 180°.
   }
   else {
     //var == 180;
@@ -142,11 +156,11 @@ void spezial_funktion_runter (int minimalwinkel, Servo *servoname, Servo *servon
 
     servoname2->write(var2 - 1);
 
-   // Serial.print(var2 - 1);
-   // Serial.print(" ");
-   // Serial.print(var - 1);
-   // Serial.print(" ");
-   // Serial.println(var + var2);
+    // Serial.print(var2 - 1);
+    // Serial.print(" ");
+    // Serial.print(var - 1);
+    // Serial.print(" ");
+    // Serial.println(var + var2);
   } else {
     //var = 11;
   }
@@ -205,113 +219,182 @@ void loop()
       }
     }
   */
-/*
-  if (Serial.available() > 0 ) {
-    speed_var_check = Serial.read();
-    Serial.println(speed_var_check);
-  }
-  switch (speed_var_check) {
-    case 49: speed_var = 5;
-    case 50: speed_var = 6;
-    case 51: speed_var = 7;
-    case 52: speed_var = 8;
-    case 53: speed_var = 9;
-    case 54: speed_var = 10;
-    case 55: speed_var = 11;
-    case 56: speed_var = 12;
-    case 57: speed_var = 13;
-    case 58: speed_var = 14;
-    case 59: speed_var = 15;
-    case 60: speed_var = 16;
-    case 61: speed_var = 17;
-    case 62: speed_var = 18;
-    case 63: speed_var = 19;
-    case 64: speed_var = 20;
-  }
+  /*
+    if (Serial.available() > 0 ) {
+      speed_var_check = Serial.read();
+      Serial.println(speed_var_check);
+    }
+    switch (speed_var_check) {
+      case 49: speed_var = 5;
+      case 50: speed_var = 6;
+      case 51: speed_var = 7;
+      case 52: speed_var = 8;
+      case 53: speed_var = 9;
+      case 54: speed_var = 10;
+      case 55: speed_var = 11;
+      case 56: speed_var = 12;
+      case 57: speed_var = 13;
+      case 58: speed_var = 14;
+      case 59: speed_var = 15;
+      case 60: speed_var = 16;
+      case 61: speed_var = 17;
+      case 62: speed_var = 18;
+      case 63: speed_var = 19;
+      case 64: speed_var = 20;
+    }
   */
-// Check if the python program sent commands
-  while(esp_server.available()) {
+  // Check if the python program sent commands
+  if (esp_server.available()) {
     // Read one line of commands
-    String command= esp_server.readStringUntil('\n');
-  }
+    String command = esp_server.readStringUntil('\n');
+    Serial.println(command);
 
+    if (command == "a") {
+      python_button_var = 97;
+    }
+    else if (command == "b") {
+      python_button_var = 98;
+    }
+    else if (command == "c") {
+      python_button_var = 99;
+    }
+    else if (command == "d") {
+      python_button_var = 100;
+    }
+    else if (command == "e") {
+      python_button_var = 101;
+    }
+    else if (command == "f") {
+      python_button_var = 102;
+    }
+    else if (command == "g") {
+      python_button_var = 103;
+    }
+    else if (command == "h") {
+      python_button_var = 104;
+    }
+    else if (command == "i") {
+      python_button_var = 105;
+    }
+    else if (command == "j") {
+      python_button_var = 106;
+    }
+    else if (command == "k") {
+      python_button_var = 107;
+    }
+    else if (command == "1") {
+      python_button_var = 49;
+    }
+    else if (command == "2") {
+      python_button_var = 50;
+    }
+    else if (command == "3") {
+      python_button_var = 51;
+    }
+    else if (command == "4") {
+      python_button_var = 52;
+    }
+    else if (command == "5") {
+      python_button_var = 53;
+    }
+    else if (command == "6") {
+      python_button_var = 54;
+    }
 
-  if (Serial.available() > 0) {
-    python_button_var = Serial.read();
+    Serial.print(" ");
+    Serial.print("Variable:");
+    
 Serial.println(python_button_var);
+
   }
-if(command=='a'){
-  python_button_var=97;
-}else if(command=='b'){
-  python_button_var=98;}
-  else if(command=='c'){
-  python_button_var=99;}
-  else if(command=='d'){
-  python_button_var=100;}
-  else if(command=='e'){
-  python_button_var=101;}
-  else if(command=='f'){
-  python_button_var=102;}
-  else if(command=='g'){
-  python_button_var=103;}
-  else if(command=='h'){
-  python_button_var=104;}
-  else if(command=='i'){
-  python_button_var=105;}
-  else if(command=='j'){
-  python_button_var=106;}
-  else if(command=='k'){
-  python_button_var=107;}
-  else if(command=='1'){
-  python_button_var=49;}
-  else if(command=='2'){
-  python_button_var=50;}
-  else if(command=='3'){
-  python_button_var=51;}
-  else if(command=='4'){
-  python_button_var=52;}
-  else if(command=='5'){
-  python_button_var=53;}
-  else if(command=='6'){
-  python_button_var=54;}
+/*
+if (python_button_var == 97) {
+  arm_hoch(180, &servo1);}
   
+else if (python_button_var == 107) {
+ arm_runter(11, &servo1);
+}
 
+
+else if (python_button_var == 107)
+{  } */
+
+
+
+
+
+
+if (python_button_var == 97) {
+  arm_hoch(180, &servo1);
+}else if (python_button_var == 98)
+{  arm_runter(11, &servo1);} 
+
+else if (python_button_var == 99)
+{  arm_hoch(180, &servo2);} 
+else if (python_button_var == 100)
+{  arm_runter(11, &servo2);} 
+ 
+else if (python_button_var == 101)
+{  arm_hoch(180, &servo3);} 
+ else if (python_button_var == 102)
+{  arm_runter(11, &servo3);} 
+ 
+else if (python_button_var == 103)
+{  arm_hoch(180, &servo4);} 
+ else if (python_button_var == 104)
+{  arm_runter(11, &servo4);} 
+ 
+else if (python_button_var == 105)
+{ spezial_funktion_hoch(180, &servo2, &servo3);} 
+ else if (python_button_var == 106)
+{ spezial_funktion_runter(11, &servo2, &servo3);} 
+ 
+else if (python_button_var == 107)
+{  } 
+
+
+
+
+
+
+ 
+
+ 
+/*
+   switch (python_button_var) {
+
+      case 97: arm_hoch(180, &servo1);   break; //a
+      case 98: arm_runter(11, &servo1);  break; //b
+
+      case 99: arm_hoch(180, &servo2);   break; //c
+      case 100: arm_runter(11, &servo2);  break; //d
+
+      case 101: arm_hoch(180, &servo3);   break; //e
+      case 102: arm_runter(11, &servo3);  break; //f
+
+      case 103: arm_hoch(180, &servo4);   break; //g
+      case 104: arm_runter(11, &servo4);  break; //h
+
+      case 105: spezial_funktion_hoch(180, &servo2, &servo3); break; //i
+      case 106: spezial_funktion_runter(11, &servo2, &servo3); break; //j
+
+      case 107: break; //default case //k
+
+
+
+
+
+      case 49: speed_var = 5; break;
+      case 50: speed_var = 8;  break;
+      case 51: speed_var = 11; break;
+      case 52: speed_var = 14; break;
+      case 53: speed_var = 17; break;
+      case 54: speed_var = 20; break;
+    }
+
+*/
 
   
-  switch (python_button_var) {
-
-    case 97: arm_hoch(180, &servo1);   break; //a
-    case 98: arm_runter(11, &servo1);  break; //b
-
-    case 99: arm_hoch(180, &servo2);   break; //c
-    case 100: arm_runter(11, &servo2);  break; //d
-
-    case 101: arm_hoch(180, &servo3);   break; //e
-    case 102: arm_runter(11, &servo3);  break; //f
-
-    case 103: arm_hoch(180, &servo4);   break; //g
-    case 104: arm_runter(11, &servo4);  break; //h
-
-    case 105: spezial_funktion_hoch(180, &servo2, &servo3); break; //i
-    case 106: spezial_funktion_runter(11, &servo2, &servo3); break; //j
-
-    case 107: break; //default case //k
-
-
-
-
-
-    case 49: speed_var = 5; break;
-    case 50: speed_var = 8;  break;
-    case 51: speed_var = 11; break;
-    case 52: speed_var = 14; break;
-    case 53: speed_var = 17; break;
-    case 54: speed_var = 20; break;
-  }
-
-
-
-
 
 
   /*
